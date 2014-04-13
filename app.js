91e175a598425aba760bfb8157a0d563e0a0b21a
Dan Heidel <dan.heidel@gmail.com>
2014-04-13 11:39:21 -0700

    env variable setup + user de-escalation

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var app = express();
-var port = process.argv[2];
 
-if(typeof port === 'undefined'){
-	console.error('no port defined!');
-	process.exit();
-}	
+var port, user;
+startup();
+
+var app = express();
 
 // all environments
 app.use(express.logger());
@@ -13,3 +11,30 @@ app.use(express.static(__dirname + '/public_html'));
 
 app.listen(port);
 console.log('serving ourstupiduniverse.net on port: ' + port);
+
+function startup(){
+  console.log('starting as user: ' + process.env.USER);
+
+  user = parseInt(process.env.NODEUSERID) || parseInt(process.argv[2]);
+  if(!user){
+    console.error('no user specified, exiting');
+    process.exit();
+  }
+
+  //attempt to de-escalate user permissions
+  try {
+    process.setgid(user);
+    process.setuid(user);
+  } catch (e) {
+    console.error('problem setting user/group, exiting');
+    console.dir(e);
+    process.exit();
+  }
+  console.log('user changed to: ' + user);
+
+  port = parseInt(process.env.NODESERVERPORT) || parseInt(process.argv[3]);
+  if(!port){
+    console.error('no port defined, exiting');
+    process.exit();
+  }
+}
